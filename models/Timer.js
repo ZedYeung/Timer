f9beed7316d90a321faf90cf6b23a2726591a110
Yue Yang <shoominga@gmail.com>
2018-04-25 14:11:13 -0700

    add mongoose model

---
+++ models/Timer.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    "title": String,
+    "project": String,
+    "elapsed": {
+        type: Number,
+        default: 0
+    },
+    "id": {
+      type: String,
+      unique: true,
+      index: true
+    },
+    "runningSince": {
+      type: Number,
+      default: null
+    }
+})
+
+module.exports = mongoose.model('Timer', schema))
